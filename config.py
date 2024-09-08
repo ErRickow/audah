2f35fd2a649cf405ce8dd6e244cd874a40af1d1a
Just R <remixingmagelang@gmail.com>
2024-09-09 02:21:11 +0700

    update config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+from base64 import b64decode as jandigantinantierornanges
+from distutils.util import strtobool
+from os import getenv
+from PyroKar.helpers.cmd import cmd
+
+from dotenv import load_dotenv
+
+load_dotenv("config.env")
