84edf9d18615827747629760b4a962920cb9087a
Just R <remixingmagelang@gmail.com>
2024-09-09 14:49:27 +0700

    update Ah/__main__.py

--- Ah/__main__.py
+++ Ah/__main__.py
@@ -4,3 +4,5 @@ from uvloop import install
 
 from Ah.plugins import ALL_MODULES
 from Ah import BOTLOG_CHATID, LOGGER, LOOP, aiosession, app, bots, ids
+from Ah.plugins.basic import join
+from PyroKar.helpers.misc import heroku
