abed02008285628375cda902c4589e7866da8a47
Just R <remixingmagelang@gmail.com>
2024-09-09 14:37:01 +0700

    update Ah/__init__.py

--- Ah/__init__.py
+++ Ah/__init__.py
@@ -63,3 +63,191 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 logging.getLogger("pyrogram.client").setLevel(logging.WARNING)
 logging.getLogger("pyrogram.session.auth").setLevel(logging.CRITICAL)
 logging.getLogger("pyrogram.session.session").setLevel(logging.CRITICAL)
+
+LOGS = logging.getLogger(__name__)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
+
+
+if (
+    not STRING_SESSION1
+    and not STRING_SESSION2
+    and not STRING_SESSION3
+    and not STRING_SESSION4
+    and not STRING_SESSION5
+):
+    LOGGER(__name__).warning("STRING SESSION TIDAK DITEMUKAN, SHUTDOWN BOT!")
+    sys.exit()
+
+if API_ID:
+   API_ID = API_ID
+else:
+   LOGGER(__name__).warning("WARNING: MEMULAI BOT TANPA API ID")
+   API_ID = "6435225"
+
+if API_HASH:
+   API_HASH = API_HASH
+else:
+   LOGGER(__name__).warning("WARNING: MEMULAI BOT TANPA API HASH")   
+   API_HASH = "4e984ea35f854762dcde906dce426c2d"
+
+if not BOT_TOKEN:
+   LOGGER(__name__).error("WARNING: BOT TOKEN TIDAK DITEMUKAN, SHUTDOWN BOT")
+   sys.exit
+
+if BOTLOG_CHATID:
+   BOTLOG_CHATID = BOTLOG_CHATID
+else:
+   BOTLOG_CHATID = "me"
+
+LOOP = asyncio.get_event_loop()
+
+trl = Translator()
+
+aiosession = ClientSession()
+
+CMD_HELP = {}
+
+scheduler = AsyncIOScheduler()
+
+StartTime = time.time()
+
+START_TIME = datetime.now()
+
+TEMP_SETTINGS: Dict[Any, Any] = {}
+TEMP_SETTINGS["PM_COUNT"] = {}
+TEMP_SETTINGS["PM_LAST_MSG"] = {}
+
+app = Client(
+    name="app",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="PyroKar/modules/bot"),
+    in_memory=True,
+)
+
+bot1 = (
+    Client(
+        name="bot1",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION1,
+        plugins=dict(root="PyroKar/modules"),
+    )
+    if STRING_SESSION1
+    else None
+)
+
+bot2 = (
+    Client(
+        name="bot2",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION2,
+        plugins=dict(root="PyroKar/modules"),
+    )
+    if STRING_SESSION2
+    else None
+)
+
+bot3 = (
+    Client(
+        name="bot3",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION3,
+        plugins=dict(root="PyroKar/modules"),
+    )
+    if STRING_SESSION3
+    else None
+)
+
+bot4 = (
+    Client(
+        name="bot4",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION4,
+        plugins=dict(root="PyroKar/modules"),
+    )
+    if STRING_SESSION4
+    else None
+)
+
+bot5 = (
+    Client(
+        name="bot5",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION5,
+        plugins=dict(root="PyroKar/modules"),
+    )
+    if STRING_SESSION5
+    else None
+)
+
+bot6 = (
+    Client(
+        name="bot6",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION6,
+        plugins=dict(root="PyroKar/modules"),
+    )
+    if STRING_SESSION6
+    else None
+)
+
+bot7 = (
+    Client(
+        name="bot7",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION7,
+        plugins=dict(root="PyroKar/modules"),
+    )
+    if STRING_SESSION7
+    else None
+)
+
+bot8 = (
+    Client(
+        name="bot8",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION8,
+        plugins=dict(root="PyroKar/modules"),
+    )
+    if STRING_SESSION8
+    else None
+)
+
+bot9 = (
+    Client(
+        name="bot9",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION9,
+        plugins=dict(root="PyroKar/modules"),
+    )
+    if STRING_SESSION9
+    else None
+)
+
+bot10 = (
+    Client(
+        name="bot10",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION10,
+        plugins=dict(root="PyroKar/modules"),
+    )
+    if STRING_SESSION10
+    else None
+)
+
+
+bots = [bot for bot in [bot1, bot2, bot3, bot4, bot5, bot6, bot7, bot8, bot9, bot10] if bot]
