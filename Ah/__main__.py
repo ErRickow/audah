f82149852c7abf5b5cc55622b6cfcdc2bedbb7ab
Just R <remixingmagelang@gmail.com>
2024-09-10 08:24:59 +0700

    update Ah/__main__.py

--- Ah/__main__.py
+++ Ah/__main__.py
@@ -7,7 +7,7 @@ from Ah import BOTLOG_CHATID, LOGGER, LOOP, aiosession, app, bots, ids
 from Ah.plugins.basic import join
 
 BOT_VER = "0.1.0"
-CMD_HANDLER = ["." "," "?" "!"]
+CMD_HANDLER = "."  # Atau gunakan CMD_HANDLER = [".", "?", "!"] jika mendukung lebih dari satu handler
 MSG_ON = """
 💢 **PyroKar Telah Hidup** 💢
 ╼┅━━━━━━━━━━╍━━━━━━━━━━┅╾
@@ -21,8 +21,11 @@ async def main():
     await app.start()
     print("LOG: Founded Bot token Booting..")
     for all_module in ALL_MODULES:
-        importlib.import_module("Ah.plugins" + all_module)
-        print(f"Successfully Imported {all_module} ")
+        try:
+            importlib.import_module("Ah.plugins." + all_module)
+            print(f"Successfully Imported {all_module} ")
+        except ModuleNotFoundError as e:
+            print(f"Failed to import {all_module}: {e}")
     for bot in bots:
         try:
             await bot.start()
@@ -30,12 +33,12 @@ async def main():
             await join(bot)
             try:
                 await bot.send_message(BOTLOG_CHATID, MSG_ON.format(BOT_VER, CMD_HANDLER))
-            except BaseException:
-                pass
+            except Exception as e:
+                print(f"Failed to send message in BOTLOG_CHATID: {e}")
             print(f"Started as {ex.first_name} | {ex.id} ")
             ids.append(ex.id)
         except Exception as e:
-            print(f"{e}")
+            print(f"Error during bot start: {e}")
     await idle()
     await aiosession.close()
 
@@ -43,5 +46,4 @@ async def main():
 if __name__ == "__main__":
     LOGGER("ErUserbot").info("Er Ubot Telah Idup")
     install()
-    heroku()
     LOOP.run_until_complete(main())
