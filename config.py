590485d478bf1f1ea854796abe6fe191a68e8d6b
Just R <remixingmagelang@gmail.com>
2024-09-11 22:09:18 +0700

    update config.py

--- config.py
+++ config.py
@@ -45,4 +45,4 @@ DEVS = [1831850761]
 BLACKLIST_GCAST = [-1001921519384, -1002053287763, -1002044997044, -1002022625433, -1002050846285]
 
 if not all([API_ID, API_HASH, SESSION]):
-    raise ValueError("Missing one or more environment variables: API_ID, API_HASH, SESSION")
+    raise ValueError("Missing one or more environment variables: API_ID, API_HASH, SESSION, BOT_TOKEN")
