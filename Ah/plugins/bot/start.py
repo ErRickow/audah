d001521705aaf8805b494762a3ee2db533f0971a
Just R <remixingmagelang@gmail.com>
2024-09-11 23:59:41 +0700

    update Ah/plugins/bot/start.py

---
+++ Ah/plugins/bot/start.py
@@ -0,0 +1,7 @@
+from Ah import ubot
+from pyrogram import filters
+
+
+@ubot.on_message(filters.command("start") & filters.private)
+async def start(client, message):
+   await message.reply_text("Mau Kau Apa Ini Kan Udah Aktif")
