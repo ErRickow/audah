f0507c86106822e536407cfda17546716b0f2a85
Just R <remixingmagelang@gmail.com>
2024-09-11 23:04:15 +0700

    update Ah/__init__.py

--- Ah/__init__.py
+++ Ah/__init__.py
@@ -1,6 +1,7 @@
 #Created By HakutakaID # TELEGRAM t.me/hakutakaid
 import logging
 import time
+import sys
 from logging.handlers import RotatingFileHandler
 from datetime import datetime
 from pyrogram import Client as mecha
