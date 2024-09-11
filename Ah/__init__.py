0dae16250d2696cb2b6dbe5077d33c65fbbd3da1
Just R <remixingmagelang@gmail.com>
2024-09-11 21:08:27 +0700

    update Ah/__init__.py

--- Ah/__init__.py
+++ Ah/__init__.py
@@ -1,78 +1,18 @@
-import asyncio
+#Created By HakutakaID # TELEGRAM t.me/hakutakaid
 import logging
-import os
-import random
-import re
-import string
-import time
-from datetime import datetime as dt
-from inspect import getfullargspec
-import sqlite3
-import platform
-import subprocess
-import asyncio
-import logging
-import sys
 import time
 from datetime import datetime
-from logging.handlers import RotatingFileHandler
-from typing import Any, Dict
-
-
-from aiohttp import ClientSession
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from gpytranslate import Translator
 from pyrogram import Client as mecha
+from pyrogram import filters as indri
+from pyrogram.handlers import CallbackQueryHandler, MessageHandler
+from config import *
 
-from config import (
-    API_HASH,
-    API_ID,
-    DB_URL,
-    BOTLOG_CHATID,
-    STRING_SESSION1,
-    STRING_SESSION2,
-    STRING_SESSION3,
-    STRING_SESSION4,
-    STRING_SESSION5,
-    STRING_SESSION6,
-    STRING_SESSION7,
-    STRING_SESSION8,
-    STRING_SESSION9,
-    STRING_SESSION10,
-    SUDO_USERS,
-    BOT_TOKEN
-)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-class Config:
-    """This class generates the configs """
-    pass
+START_TIME = datetime.now()
 
-
-def isLocalHost():
-    """Check if it is localhost"""
-    return os.path.exists(".env")
-
-LOG_FILE_NAME = "logs.txt"
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="[%(levelname)s] - %(name)s - %(message)s",
-    datefmt="%d-%b-%y %H:%M:%S",
-    handlers=[
-        RotatingFileHandler(LOG_FILE_NAME, maxBytes=50000000, backupCount=10),
-        logging.StreamHandler(),
-    ],
-)
-logging.getLogger("asyncio").setLevel(logging.CRITICAL)
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-logging.getLogger("pyrogram.mecha").setLevel(logging.WARNING)
-logging.getLogger("pyrogram.session.auth").setLevel(logging.CRITICAL)
-logging.getLogger("pyrogram.session.session").setLevel(logging.CRITICAL)
-
-LOGS = logging.getLogger(__name__)
-
-def LOGGER(name: str) -> logging.Logger:
-    return logging.getLogger(name)
+StartTime = time.time()
 
 class Ubot(mecha):
     def __init__(self, **kwargs):
@@ -89,176 +29,12 @@ class Ubot(mecha):
         await super().start()
         logger.info("Bot started")
 
-if (
-    not STRING_SESSION1
-    and not STRING_SESSION2
-    and not STRING_SESSION3
-    and not STRING_SESSION4
-    and not STRING_SESSION5
-):
-    LOGGER(__name__).warning("STRING SESSION TIDAK DITEMUKAN, SHUTDOWN BOT!")
-    sys.exit()
-
-if API_ID:
-   API_ID = API_ID
-else:
-   LOGGER(__name__).warning("WARNING: MEMULAI BOT TANPA API ID")
-   API_ID = "6435225"
-
-if API_HASH:
-   API_HASH = API_HASH
-else:
-   LOGGER(__name__).warning("WARNING: MEMULAI BOT TANPA API HASH")   
-   API_HASH = "4e984ea35f854762dcde906dce426c2d"
-
-if not BOT_TOKEN:
-   LOGGER(__name__).error("WARNING: BOT TOKEN TIDAK DITEMUKAN, SHUTDOWN BOT")
-   sys.exit
-
-if BOTLOG_CHATID:
-   BOTLOG_CHATID = BOTLOG_CHATID
-else:
-   BOTLOG_CHATID = "me"
-
-LOOP = asyncio.get_event_loop()
-
-aiosession = ClientSession()
-
-scheduler = AsyncIOScheduler()
-
-StartTime = time.time()
-
-START_TIME = datetime.now()
-
-app = mecha(
-    name="app",
+ubot = Ubot(
+    name="sange",
     api_id=API_ID,
     api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
-    plugins=dict(root="Ah/plugins/bot"),
+    session_string=SESSION,
     in_memory=True,
 )
 
-bot1 = (
-    mecha(
-        name="bot1",
-        api_id=API_ID,
-        api_hash=API_HASH,
-        session_string=STRING_SESSION1,
-        plugins=dict(root="Ah/plugins"),
-    )
-    if STRING_SESSION1
-    else None
-)
-
-bot2 = (
-    mecha(
-        name="bot2",
-        api_id=API_ID,
-        api_hash=API_HASH,
-        session_string=STRING_SESSION2,
-        plugins=dict(root="Ah/plugins"),
-    )
-    if STRING_SESSION2
-    else None
-)
-
-bot3 = (
-    mecha(
-        name="bot3",
-        api_id=API_ID,
-        api_hash=API_HASH,
-        session_string=STRING_SESSION3,
-        plugins=dict(root="Ah/plugins"),
-    )
-    if STRING_SESSION3
-    else None
-)
-
-bot4 = (
-    mecha(
-        name="bot4",
-        api_id=API_ID,
-        api_hash=API_HASH,
-        session_string=STRING_SESSION4,
-        plugins=dict(root="Ah/plugins"),
-    )
-    if STRING_SESSION4
-    else None
-)
-
-bot5 = (
-    mecha(
-        name="bot5",
-        api_id=API_ID,
-        api_hash=API_HASH,
-        session_string=STRING_SESSION5,
-        plugins=dict(root="Ah/plugins"),
-    )
-    if STRING_SESSION5
-    else None
-)
-
-bot6 = (
-    mecha(
-        name="bot6",
-        api_id=API_ID,
-        api_hash=API_HASH,
-        session_string=STRING_SESSION6,
-        plugins=dict(root="Ah/plugins"),
-    )
-    if STRING_SESSION6
-    else None
-)
-
-bot7 = (
-    mecha(
-        name="bot7",
-        api_id=API_ID,
-        api_hash=API_HASH,
-        session_string=STRING_SESSION7,
-        plugins=dict(root="Ah/plugins"),
-    )
-    if STRING_SESSION7
-    else None
-)
-
-bot8 = (
-    mecha(
-        name="bot8",
-        api_id=API_ID,
-        api_hash=API_HASH,
-        session_string=STRING_SESSION8,
-        plugins=dict(root="Ah/plugins"),
-    )
-    if STRING_SESSION8
-    else None
-)
-
-bot9 = (
-    mecha(
-        name="bot9",
-        api_id=API_ID,
-        api_hash=API_HASH,
-        session_string=STRING_SESSION9,
-        plugins=dict(root="Ah/plugins"),
-    )
-    if STRING_SESSION9
-    else None
-)
-
-bot10 = (
-    mecha(
-        name="bot10",
-        api_id=API_ID,
-        api_hash=API_HASH,
-        session_string=STRING_SESSION10,
-        plugins=dict(root="Ah/plugins"),
-    )
-    if STRING_SESSION10
-    else None
-)
-
-
-bots = [bot for bot in [bot1, bot2, bot3, bot4, bot5, bot6, bot7, bot8, bot9, bot10] if bot]
-from Ah import *
+from Ah.bantuan import *
