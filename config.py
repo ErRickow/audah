46c5d950b3546a407a922b0b904a327b8e9c40e4
Just R <remixingmagelang@gmail.com>
2024-09-11 21:04:35 +0700

    update config.py

--- config.py
+++ config.py
@@ -1,43 +1,19 @@
 import os
-from base64 import b64decode as jandigantinantierornanges
-from distutils.util import strtobool
-from os import getenv
-from Ah.bantuan.cmd import cmd
 from dotenv import load_dotenv
 
 load_dotenv(".env")
 
 API_ID = os.getenv("API_ID")
+
 API_HASH = os.getenv("API_HASH")
-BLACKLIST_CHAT = getenv("BLACKLIST_CHAT", None)
-if not BLACKLIST_CHAT:
-    BLACKLIST_CHAT = [-1001473548283, -1001812143750]
-BLACKLIST_GCAST = {int(x) for x in getenv("BLACKLIST_GCAST", "").split() if x}
-BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
-BOT_VER = "1.1.5@main"
-BRANCH = getenv("BRANCH", "main")
-CMD_HNDLR = cmd
-OWNER_ID = getenv("OWNER_ID", "")
-BOT_TOKEN = getenv("BOT_TOKEN")
-CHANNEL = getenv("CHANNEL", "Pamerdong")
-CMD_HANDLER = getenv("CMD_HANDLER", ".")
-DB_URL = getenv("DATABASE_URL", "sqlite:///tron.db")
-GIT_TOKEN = getenv(
-    "GIT_TOKEN",
-    jandigantinantierornanges("").decode("utf-8"),
-)
-GROUP = getenv("GROUP", "obrolansuar")
-PMPERMIT_PIC = getenv("PMPERMIT_PIC", None)
-PM_AUTO_BAN = strtobool(getenv("PM_AUTO_BAN", "True"))
-REPO_URL = getenv("REPO_URL", "https://github.com/ErRickow/audah")
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
-STRING_SESSION2 = getenv("STRING_SESSION2", "")
-STRING_SESSION3 = getenv("STRING_SESSION3", "")
-STRING_SESSION4 = getenv("STRING_SESSION4", "")
-STRING_SESSION5 = getenv("STRING_SESSION5", "")
-STRING_SESSION6 = getenv("STRING_SESSION6", "")
-STRING_SESSION7 = getenv("STRING_SESSION7", "")
-STRING_SESSION8 = getenv("STRING_SESSION8", "")
-STRING_SESSION9 = getenv("STRING_SESSION9", "")
-STRING_SESSION10 = getenv("STRING_SESSION10", "")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+
+SESSION = os.getenv("SESSION")
+
+prefix = [".", "", "-"]
+
+DEVS = [1831850761]
+
+BLACKLIST_GCAST = [-1001921519384, -1002053287763, -1002044997044, -1002022625433, -1002050846285]
+
+if not all([API_ID, API_HASH, SESSION]):
+    raise ValueError("Missing one or more environment variables: API_ID, API_HASH, SESSION")
