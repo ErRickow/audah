ace5cfbf1481f2bc101ecaa38afcb7efc3376c8b
Just R <remixingmagelang@gmail.com>
2024-09-12 01:04:32 +0700

    update Ah/plugins/__init__.py

--- Ah/plugins/__init__.py
+++ Ah/plugins/__init__.py
@@ -1,13 +1,13 @@
-#Created By HakutakaID # TELEGRAM t.me/hakutakaid
 import glob
-from os.path import basename, dirname, isfile
+from os.path import dirname, isfile
 
 
 def __list_all_modules():
-    mod_paths = glob.glob(dirname(__file__) + "/*.py")
+    work_dir = dirname(__file__)
+    mod_paths = glob.glob(work_dir + "/*/*.py")
 
     all_modules = [
-        basename(f)[:-3]
+        (((f.replace(work_dir, "")).replace("/", "."))[:-3])
         for f in mod_paths
         if isfile(f)
         and f.endswith(".py")
