fe82f8cf105a914cb5a7f1dc2d0afad9ca22b3aa
Just R <remixingmagelang@gmail.com>
2024-09-13 00:46:07 +0700

    update Ah/plugins/up.py

--- Ah/plugins/up.py
+++ Ah/plugins/up.py
@@ -60,4 +60,4 @@ async def ngapdate(client, message):
         
     await pros.edit(f"<b>{memeg}</b><br><br>{teks}{format_output}<br><b>Last Commit:</b> {last_commit}")
     
-    os.execl(sys.executable, sys.executable, "-m" "Ah")
+    os.execl(sys.executable, sys.executable, "-m", "Ah")
