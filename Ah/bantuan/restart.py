7c90120c01b1e0fd651cdd75f3784cada659204e
Just R <remixingmagelang@gmail.com>
2024-09-12 22:10:39 +0700

    update Ah/bantuan/restart.py

---
+++ Ah/bantuan/restart.py
@@ -0,0 +1,34 @@
+import asyncio
+import contextlib
+import math
+import os
+import shlex
+import shutil
+import time
+
+async def restart(
+    update: bool = True,
+    clean_up: bool = False,
+    shutdown: bool = False,
+):
+    try:
+        shutil.rmtree(Config.DWL_DIR)
+        shutil.rmtree(Config.TEMP_DIR)
+    except BaseException:
+        pass
+
+    if clean_up:
+        os.system(f"mkdir {Config.DWL_DIR}")
+        os.system(f"mkdir {Config.TEMP_DIR}")
+        return
+
+    if shutdown:
+        return os.system(f"kill -9 {os.getpid()}")
+
+    cmd = (
+        "git pull && pip3 install -U -r requirements.txt && bash start.sh"
+        if update
+        else "bash start.sh"
+    )
+
+    os.system(f"kill -9 {os.getpid()} && {cmd}")
