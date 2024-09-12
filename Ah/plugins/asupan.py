ec670fbcaef1af9070d1d921795a7ed138363271
Just R <remixingmagelang@gmail.com>
2024-09-13 01:47:03 +0700

    update Ah/plugins/asupan.py

--- Ah/plugins/asupan.py
+++ Ah/plugins/asupan.py
@@ -4,7 +4,7 @@ from random import choice
 from pyrogram import Client, enums, filters
 from pyrogram.types import Message
 
-from config import CMD_HANDLER as cmd
+from config import PREFIX as cmd
 from Ah.bantuan.basic import edit_or_reply
 from Ah.bantuan.PyroHelpers import ReplyCheck
 
