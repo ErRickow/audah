2ed9659c8c7c4b288cdb73dc8f2cb297155c9325
Just R <remixingmagelang@gmail.com>
2024-09-13 04:17:35 +0700

    update Ah/plugins/gemini.py

--- Ah/plugins/gemini.py
+++ Ah/plugins/gemini.py
@@ -1,40 +1,47 @@
-import os
-
-from pyrogram import Client, filters, enums
+import requests
+from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from Ah.bantuan.tools import import_library
-from config import gemini_key
+from Ah import *
 
-import google.generativeai as genai
+async def chatgptold(messagestr):
+    url = "https://api.botcahx.eu.org/api/search/blackbox-chat"
+    payload = {
+        "text": messagestr,
+        "apikey": "LwulPck3"
+    }
+    response = requests.get(url, params=payload)
+    if response.status_code != 200:
+        return None
+    return response.json()
 
-genai.configure(api_key=gemini_key)
-
-model = genai.GenerativeModel("gemini-1.5-flash-latest")
-
-
-@Client.on_message(filters.command("gemini", prefix) & filters.me)
-async def say(_, message: Message):
+@Client.on_message(filters.command("ask", prefix) & filters.me)
+async def chatgpt_old_(client: Client, message: Message):
+    if len(message.command) > 1:
+        prompt = message.text.split(maxsplit=1)[1]
+    elif message.reply_to_message:
+        prompt = message.reply_to_message.text
+    else:
+        return await message.reply_text("Give ask from CHATGPT-3")
+    
     try:
-        await message.edit_text("<code>Please Wait...</code>")
+        messager = await chatgptold(prompt)
+        if messager is None:
+            return await message.reply_text("No response")
+        
+        # Asumsikan struktur respons dari API baru ini sesuai dengan yang Anda butuhkan
+        output = messager.get("result")  # Ganti "result" dengan kunci yang sesuai dari API baru
 
-        if len(message.command) > 1:
-            prompt = message.text.split(maxsplit=1)[1]
-        elif message.reply_to_message:
-            prompt = message.reply_to_message.text
-        else:
-            await message.edit_text(
-                f"<b>Usage: </b><code>{prefix}gemini [prompt/reply to message]</code>"
+        if len(output) > 4096:
+            with open("chat.txt", "w+", encoding="utf8") as out_file:
+                out_file.write(output)
+            await message.reply_document(
+                document="chat.txt",
+                disable_notification=True
             )
-            return
-
-        chat = model.start_chat()
-        response = chat.send_message(prompt)
-
-        await message.edit_text(
-            f"**Question:**`{prompt}`\n**Answer:** {response.text}",
-            parse_mode=enums.ParseMode.MARKDOWN,
-        )
+            os.remove("chat.txt")
+        else:
+            await message.reply_text(output)
     except Exception as e:
-        await message.edit_text(f"An error occurred: {e}")
-
+        # Kirim pesan kesalahan ke pengguna
+        await message.reply_text(f"Terjadi kesalahan: {str(e)}")
