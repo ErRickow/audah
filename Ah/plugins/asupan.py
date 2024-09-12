2f28a967b6927e7ff818ec0860d72cce892f35ff
Just R <remixingmagelang@gmail.com>
2024-09-13 01:50:25 +0700

    update Ah/plugins/asupan.py

--- Ah/plugins/asupan.py
+++ Ah/plugins/asupan.py
@@ -15,7 +15,7 @@ from .help import add_command_help
 async def asupan_cmd(client: Client, message: Message):
     Man = await message.reply(message, "`Tunggu Sebentar...`")
     await gather(
-        Man.delete(),
+#        Man.delete(),
         client.send_video(
             message.chat.id,
             choice(
