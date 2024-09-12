c31ccadc2f285f0f1abcdb2bbee41c33c4e05ef0
Just R <remixingmagelang@gmail.com>
2024-09-12 23:11:40 +0700

    update Ah/plugins/ping.py

--- Ah/plugins/ping.py
+++ Ah/plugins/ping.py
@@ -6,7 +6,7 @@ from Ah.bantuan.tools import *
 
 @Client.on_message(filters.me & filters.command("ping", cmd))
 async def pingme(client, message):
-    uptime = await readable_time((time.time() - StartTime))
+    uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
     end = datetime.now()
     duration = (end - start).microseconds / 1000
