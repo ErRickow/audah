1241288ace2217470f1d14c512be881bd84d1270
Just R <remixingmagelang@gmail.com>
2024-09-13 02:43:39 +0700

    update Ah/bantuan/SQL/chatbot_db.py

--- Ah/bantuan/SQL/chatbot_db.py
+++ Ah/bantuan/SQL/chatbot_db.py
@@ -2,26 +2,17 @@ from sqlalchemy import Boolean, Column, String, UnicodeText
 
 from Ah.bantuan.SQL import BASE, SESSION
 
-class Chatbot:
-    def __init__(self, db_name):
-        self.connection = sqlite3.connect(db_name)
-        self.cursor = self.connection.cursor()
-        self.create_table()
+from sqlalchemy.orm import sessionmaker
+from models import Chatbot, db_connect, create_table
 
-    def create_table(self):
-        self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS chatbots (
-                chat_id INTEGER PRIMARY KEY,
-                user_id INTEGER
-            )
-        ''')
-        self.connection.commit()
+class Chatbot:
+    def __init__(self):
+        engine = db_connect()
+        create_table(engine)
+        Session = sessionmaker(bind=engine)
+        self.session = Session()
 
     async def add_chatbot(self, chat_id, user_id):
-        self.cursor.execute('''
-            INSERT INTO chatbots (chat_id, user_id)
-            VALUES (?, ?)
-            ON CONFLICT(chat_id) DO UPDATE SET
-                user_id=excluded.user_id
-        ''', (chat_id, user_id))
-        self.connection.commit()
+        chatbot = Chatbot(chat_id=chat_id, user_id=user_id)
+        self.session.merge(chatbot)
+        self.session.commit()
