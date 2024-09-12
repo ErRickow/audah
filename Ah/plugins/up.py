b3aa50fcb4494fb27a307e5b12e690c55598756c
Just R <remixingmagelang@gmail.com>
2024-09-13 00:38:54 +0700

    update Ah/plugins/up.py

--- Ah/plugins/up.py
+++ Ah/plugins/up.py
@@ -1,230 +1,52 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-# Ported by @mrismanaziz
-# FROM PyroMan-Userbot < https://github.com/mrismanaziz/PyroMan-Userbot/ >
-# t.me/Lunatic0de & t.me/SharingUserbot
-#
-
+import re
 import asyncio
-import socket
+import os
 import sys
-from datetime import datetime
-from os import environ, execle, path, remove
+import shutil
+import subprocess
 
 from git import Repo
-from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
+from git.exc import InvalidGitRepositoryError
+
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from config import BRANCH
-from config import PREFIX as cmd
-from config import GIT_TOKEN, REPO_URL
-from Ah.bantuan.adminHelpers import DEVS
-from Ah.bantuan.basic import edit_or_reply
-from Ah.bantuan.misc import HAPP, XCB
-from Ah.bantuan.tools import get_arg
-from Ah.utils.misc import restart
-from Ah.utils.pastebin import PasteBin
-from Ah.utils.tools import bash
-
-from .help import add_command_help
-
-if GIT_TOKEN:
-    GIT_USERNAME = REPO_URL.split("com/")[1].split("/")[0]
-    TEMP_REPO = REPO_URL.split("https://")[1]
-    UPSTREAM_REPO = f"https://{GIT_USERNAME}:{GIT_TOKEN}@{TEMP_REPO}"
-    UPSTREAM_REPO_URL = UPSTREAM_REPO
-else:
-    UPSTREAM_REPO_URL = REPO_URL
-
-requirements_path = path.join(
-    path.dirname(path.dirname(path.dirname(__file__))), "requirements.txt"
-)
+from Ah import *
 
 
-async def gen_chlog(repo, diff):
-    ch_log = ""
-    d_form = "%d/%m/%y"
-    for c in repo.iter_commits(diff):
-        ch_log += (
-            f"• [{c.committed_datetime.strftime(d_form)}]: {c.summary} <{c.author}>\n"
-        )
-    return ch_log
+def check_command(command):
+    return shutil.which(command) is not None
 
-
-async def updateme_requirements():
-    reqs = str(requirements_path)
-    try:
-        process = await asyncio.create_subprocess_shell(
-            " ".join([sys.executable, "-m", "pip", "install", "-r", reqs]),
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
-        )
-        await process.communicate()
-        return process.returncode
-    except Exception as e:
-        return repr(e)
-
-
-@Client.on_message(
-    filters.command("diupdate", ["."]) & filters.user(DEVS) & ~filters.me
-)
-@Client.on_message(filters.command("update", cmd) & filters.me)
-async def upstream(client: Client, message: Message):
-    status = await edit_or_reply(message, "`Mengecek Pembaruan, Tunggu Sebentar...`")
-    conf = get_arg(message)
-    off_repo = UPSTREAM_REPO_URL
-    try:
-        txt = (
-            "**Pembaruan Tidak Dapat Di Lanjutkan Karna "
-            + "Terjadi Beberapa ERROR**\n\n**LOGTRACE:**\n"
-        )
-        repo = Repo()
-    except NoSuchPathError as error:
-        await status.edit(f"{txt}\n**Directory** `{error}` **Tidak Dapat Di Temukan.**")
-        repo.__del__()
-        return
-    except GitCommandError as error:
-        await status.edit(f"{txt}\n**Kegagalan awal!** `{error}`")
-        repo.__del__()
-        return
-    except InvalidGitRepositoryError:
-        if conf != "deploy":
-            pass
-        repo = Repo.init()
-        origin = repo.create_remote("upstream", off_repo)
-        origin.fetch()
-        repo.create_head(
-            BRANCH,
-            origin.refs[BRANCH],
-        )
-        repo.heads[BRANCH].set_tracking_branch(origin.refs[BRANCH])
-        repo.heads[BRANCH].checkout(True)
-    ac_br = repo.active_branch.name
-    if ac_br != BRANCH:
-        await status.edit(
-            f"**[UPDATER]:** `Looks like you are using your own custom branch ({ac_br}). in that case, Updater is unable to identify which branch is to be merged. please checkout to main branch`"
-        )
-        repo.__del__()
-        return
-    try:
-        repo.create_remote("upstream", off_repo)
-    except BaseException:
-        pass
-    ups_rem = repo.remote("upstream")
-    ups_rem.fetch(ac_br)
-    changelog = await gen_chlog(repo, f"HEAD..upstream/{ac_br}")
-    if "deploy" not in conf:
-        if changelog:
-            changelog_str = f"**Tersedia Pembaruan Untuk Branch [{ac_br}]:\n\nCHANGELOG:**\n\n`{changelog}`"
-            if len(changelog_str) > 4096:
-                await status.edit("**Changelog terlalu besar, dikirim sebagai file.**")
-                file = open("output.txt", "w+")
-                file.write(changelog_str)
-                file.close()
-                await client.send_document(
-                    message.chat.id,
-                    "output.txt",
-                    caption=f"**Ketik** `{cmd}update deploy` **Untuk Mengupdate Userbot.**",
-                    reply_to_message_id=status.id,
-                )
-                remove("output.txt")
-            else:
-                return await status.edit(
-                    f"{changelog_str}\n**Ketik** `{cmd}update deploy` **Untuk Mengupdate Userbot.**",
-                    disable_web_page_preview=True,
-                )
-        else:
-            await status.edit(
-                f"\n`Your BOT is`  **up-to-date**  `with branch`  **[{ac_br}]**\n",
-                disable_web_page_preview=True,
-            )
-            repo.__del__()
-            return
-
-        try:
-            ups_rem.pull(ac_br)
-        except GitCommandError:
-            repo.git.reset("--hard", "FETCH_HEAD")
-        await updateme_requirements()
-        await status.edit(
-            "`PyroKar-Userbot Berhasil Diupdate! Userbot bisa di Gunakan Lagi.`",
-        )
-        args = [sys.executable, "-m", "PyroKar"]
-        execle(sys.executable, *args, environ)
-        return
-
-
-@Client.on_message(filters.command("cupdate", ["."]) & filters.user(DEVS) & ~filters.me)
-@Client.on_message(filters.command("goupdate", cmd) & filters.me)
-async def updaterman(client: Client, message: Message):
-    if await is_heroku():
-        if HAPP is None:
-            return await edit_or_reply(
-                message,
-                "Pastikan HEROKU_API_KEY dan HEROKU_APP_NAME anda dikonfigurasi dengan benar di config vars heroku",
-            )
-    response = await edit_or_reply(message, "Checking for available updates...")
-    try:
-        repo = Repo()
-    except GitCommandError:
-        return await response.edit("Git Command Error")
-    except InvalidGitRepositoryError:
-        return await response.edit("Invalid Git Repsitory")
-    to_exc = f"git fetch origin {BRANCH} &> /dev/null"
-    await bash(to_exc)
-    await asyncio.sleep(7)
-    verification = ""
-    REPO_ = repo.remotes.origin.url.split(".git")[0]  # main git repository
-    for checks in repo.iter_commits(f"HEAD..origin/{BRANCH}"):
-        verification = str(checks.count())
-    if verification == "":
-        return await response.edit("Bot is up-to-date!")
-    updates = ""
-    ordinal = lambda format: "%d%s" % (
-        format,
-        "tsnrhtdd"[(format // 10 % 10 != 1) * (format % 10 < 4) * format % 10 :: 4],
+@Client.on_message(filters.command("up", prefix) & filters.me)
+async def ngapdate(client, message):
+  pros = await message.reply(
+        f"<blockquote> <b>Memeriksa pembaruan resources {client.me.mention}</b></blockquote>"
     )
-    for info in repo.iter_commits(f"HEAD..origin/{BRANCH}"):
-        updates += f"<b>➣ #{info.count()}: [{info.summary}]({REPO_}/commit/{info}) by -> {info.author}</b>\n\t\t\t\t<b>➥ Commited on:</b> {ordinal(int(datetime.fromtimestamp(info.committed_date).strftime('%d')))} {datetime.fromtimestamp(info.committed_date).strftime('%b')}, {datetime.fromtimestamp(info.committed_date).strftime('%Y')}\n\n"
-    _update_response_ = "<b>A new update is available for the Bot!</b>\n\n➣ Pushing Updates Now</code>\n\n**<u>Updates:</u>**\n\n"
-    _final_updates_ = _update_response_ + updates
-    if len(_final_updates_) > 4096:
-        url = await PasteBin(updates)
-        nrs = await response.edit(
-            f"<b>A new update is available for the Bot!</b>\n\n➣ Pushing Updates Now</code>\n\n**<u>Updates:</u>**\n\n[Click Here to checkout Updates]({url})"
+  out = subprocess.check_output(["git", "pull"]).decode("UTF-8")
+  teks = f"<b>❒ Status resources :</b>\n"
+  memeg = f"<b>Perubahan logs by {client.me.mention}</b>"
+  if "Already up to date." in str(out):
+        return await pros.edit(f"<blockquote>{teks}┖ {out}</blockquote>")
+  if len(out) > 4096:
+          anuk = await pros.edit(
+            f"<blockquote> <b>Hasil akan dikirimkan dalam bentuk file ..</b></blockquote>"
         )
-    else:
-        nrs = await response.edit(_final_updates_, disable_web_page_preview=True)
-    await bash("git stash &> /dev/null && git pull")
-    if await is_heroku():
-        try:
-            await response.edit(
-                f"{nrs.text}\n\nBot was updated successfully on Heroku! Now, wait for 2 - 3 mins until the bot restarts!"
-            )
-            await bash(
-                f"{XCB[5]} {XCB[7]} {XCB[9]}{XCB[4]}{XCB[0]*2}{XCB[6]}{XCB[4]}{XCB[8]}{XCB[1]}{XCB[5]}{XCB[2]}{XCB[6]}{XCB[2]}{XCB[3]}{XCB[0]}{XCB[10]}{XCB[2]}{XCB[5]} {XCB[11]}{XCB[4]}{XCB[12]}"
-            )
-            return
-        except Exception as err:
-            return await response.edit(f"{nrs.text}\n\nERROR: <code>{err}</code>")
-    else:
-        await bash("pip3 install -r requirements.txt")
-        restart()
-        exit()
+  anuk = None
+  with open("output.txt", "w+") as file:
+            file.write(out)
 
-
-add_command_help(
-    "update",
-    [
-        ["update", "Untuk melihat list pembaruan terbaru dari PyroKar-Userbot."],
-        ["update deploy", "Untuk mengupdate userbot."],
-    ],
-)
+           # X = f"<blockquote> <b>Perubahan logs </b></blockquote>"
+  #await client.send_document(
+  #        message.chat.id,
+  #        "output.txt",
+  #        caption=f"{X}",
+   #       reply_to_message_id=message.id,
+   #       )
+  os.remove("output.txt")
+  format_line = [f"┣ {line}" for line in out.splitlines()]
+  if format_line:
+    format_line[-1] = f"┖ {format_line[-1][2:]}"
+    format_output = "\n".join(format_line)
+  await pros.edit(f"<blockquote>{memeg}\n\n{teks}{format_output}\n\nZzzzz LuciferReborns</blockquote>")
+  os.execl(sys.executable, sys.executable, "erbanget.py")
+  
