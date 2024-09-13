0976cb4a9cd24a0480e19c9d29bd7bca1364f7d7
Just R <remixingmagelang@gmail.com>
2024-09-13 07:08:20 +0700

    update Ah/plugins/luminai.py

--- Ah/plugins/luminai.py
+++ Ah/plugins/luminai.py
@@ -12,3 +12,10 @@ async def luminer(messagestr):
     
 @Client.on_message(filters.me & filters.command("uy", cmd))
 async def saya(client: Client, message: Message)
+    if len(message.command) > 1:
+        prompt = message.text.split(maxsplit=1)[1]
+    elif message.reply_to_message:
+        prompt = message.reply_to_message.text
+    else:
+        return await message.reply_text("Give ask from CHATGPT-3")
+    try:
