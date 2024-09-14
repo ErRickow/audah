088b6ccac88ec791e8546c53f81c77089a6f69f3
Just R <remixingmagelang@gmail.com>
2024-09-15 06:30:36 +0700

    update Ah/plugins/bangsat.py

---
+++ Ah/plugins/bangsat.py
@@ -0,0 +1,49 @@
+import requests
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from Ah.bantuan.tools import *
+from config import PREFIX as cmd
+from Ah import *
+
+chatbot_active = False
+
+def send_simtalk(message: str) -> str:
+    if len(message) > 1000:
+        return "Character terlalu panjang."
+    else:
+        params = {"text": message, "lc": "id"}
+        try:
+            response = requests.post(
+                "https://api.simsimi.vn/v1/simtalk",
+                data=params
+            ).json()
+            return response.get("message", "Maaf, tidak bisa merespons sekarang.")
+        except Exception as e:
+            return f"Error: {str(e)}"
+
+
+@Client.on_message(filters.text & ~filters.bot & filters.me)
+async def chatbot_response(client, message: Message):
+    global chatbot_active
+
+    if not chatbot_active:
+        return
+    text = message.text
+
+    if not text:
+        return
+    simtalk_response = send_simtalk(text)
+    await message.reply(simtalk_response)
+
+
+@Client.on_message(filters.command("chatbot") & filters.me)
+async def manage_chatbot_status(client, message: Message):
+    global chatbot_active
+    arg = get_text(message).lower()
+
+    if arg == "off":
+        chatbot_active = False
+        await message.reply("Chatbot dinonaktifkan.")
+    elif arg == "on":
+        chatbot_active = True
+        await message.reply("Chatbot diaktifkan.")
