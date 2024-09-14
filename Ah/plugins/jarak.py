6743b64d94d69212ab423778cc11568847579f30
Just R <remixingmagelang@gmail.com>
2024-09-14 23:29:59 +0700

    update Ah/plugins/jarak.py

--- Ah/plugins/jarak.py
+++ Ah/plugins/jarak.py
@@ -1,4 +1,8 @@
-eval import requests
+import requests
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from Ah import *
+from Ah.bantuan.tools import *
 
 async def tanya(asal, tujuan):
     url = "https://widipe.com/jarak"
@@ -12,17 +16,22 @@ async def tanya(asal, tujuan):
         if 'url' in data and 'data' in data['url']:
             gambar_url = data['url']['data']
             deskripsi = data['url']['desc']
+            return gambar_url, deskripsi
+    return None, None
 
 @Client.on_message(filters.command("jarak", cmd))
 async def mbuh(client, message: Message):
-    text = get_text(message)
-    if not tujuan:
-        return await message.reply("Kasih teks GOLBOK!!")
-    await message.reply_photo(photo=gambar_url, caption=f"> Jarak dari {asal} ke {tujuan} = <code>{deskripsi}</code>")
-        else:
-           await message.reply("Gambar tidak tersedia.")
+    text = message.text.split(" ")
+    
+    if len(text) < 3:
+        return await message.reply("Kasih teks GOBLOK!! Contoh: /jarak asal tujuan")
+    
+    asal = text[1]
+    tujuan = text[2]
+    
+    gambar_url, deskripsi = await tanya(asal, tujuan)
+
+    if gambar_url:
+        await message.reply_photo(photo=gambar_url, caption=f"> Jarak dari {asal} ke {tujuan} = <code>{deskripsi}</code>")
     else:
-      await message.reply("Tidak ada hasil.")
-    asal = tanya(asal)
-    tujuan = tanya(tujuan)
-    await tanya(asal, tujuan)
+        await message.reply("Gambar tidak tersedia atau tidak ada hasil.")
