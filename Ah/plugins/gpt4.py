a0117a0594edffdac6dc0045a7a58502dca1ea0b
Just R <remixingmagelang@gmail.com>
2024-09-14 20:36:29 +0700

    update Ah/plugins/gpt4.py

---
+++ Ah/plugins/gpt4.py
@@ -0,0 +1,30 @@
+import requests
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from Ah.bantuan.tools import *
+
+from Ah import *
+
+async def tanya(text):
+    url = "https://widipe.com/gpt4"
+    params = {'text': text}
+    headers = {'accept': 'application/json'}
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+          # Memastikan status code 200
+        data = response.json()
+    if 'result' in data:
+        return data['result']
+    else:
+        return f"{response.text}"
+#    except requests.exceptions.RequestException as e:
+#        return f"Error: {str(e)}"
+
+@Client.on_message(filters.command("gtp", cmd))
+async def gtp(client, message: Message):
+    text = get_text(message)
+    if not text:
+        return await message.reply("Kasih teks GOLBOK!!")
+    pros = await message.reply("Sabar njing ..")
+    hasil = await tanya(text)
+    return await pros.edit(hasil)
