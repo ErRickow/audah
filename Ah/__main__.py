6f83035c3b9fffbdbebb44a7715e5dfbf191eb5a
Just R <remixingmagelang@gmail.com>
2024-09-15 05:58:07 +0700

    update Ah/__main__.py

--- Ah/__main__.py
+++ Ah/__main__.py
@@ -48,5 +48,6 @@ async def start_bot():
 
 if __name__ == "__main__":
     logger.info("Starting bot")
+    LOOP.run_until_complete(main())
     asyncio.get_event_loop().run_until_complete(start_bot())
     logger.info("Bot stopped")
