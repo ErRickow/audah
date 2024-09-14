15e36cb5c62299f181ea37738044447e2402ef82
Just R <remixingmagelang@gmail.com>
2024-09-15 06:36:06 +0700

    update Ah/__main__.py

--- Ah/__main__.py
+++ Ah/__main__.py
@@ -23,34 +23,31 @@ MSG_ON = """
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-async def start_bot():
-    await ubot.start()
+async def main():
+    await app.start()
     print("LOG: Founded Bot token Booting..")
-    for module in tqdm(ALL_MODULES, desc="Loading modules", unit="module"):
-        importlib.import_module("Ah.plugins" + module)
-        logger.info(f"Module {module} imported")
-    
+    for all_module in ALL_MODULES:
+        importlib.import_module("PyroKar.modules" + all_module)
+        print(f"Successfully Imported {all_module} ")
     for bot in bots:
         try:
             await bot.start()
             ex = await bot.get_me()
             await join(bot)
             try:
-                await bot.send_message(BOTLOG, MSG_ON.format(BOT_VER, PREFIX))
+                await bot.send_message(BOTLOG_CHATID, MSG_ON.format(BOT_VER, CMD_HANDLER))
             except BaseException:
                 pass
-            print(f"Started as {ex.first_name} | {ex.id}")
+            print(f"Started as {ex.first_name} | {ex.id} ")
             ids.append(ex.id)
         except Exception as e:
             print(f"{e}")
-        await asyncio.sleep(5)
     await idle()
-    ClientSession.close()
-    logger.info("Bot is sange")
+    await aiosession.close()
+
 
 if __name__ == "__main__":
-    logger.info("Starting bot")
+    LOGGER("Er Anjing").info("The-Ubot Telah Hidup")
     install()
-    LOOP.run_until_complete(start_bot())
-#    asyncio.get_event_loop().run_until_complete(start_bot())
-    logger.info("Bot stopped")
+    heroku()
+    LOOP.run_until_complete(main())
