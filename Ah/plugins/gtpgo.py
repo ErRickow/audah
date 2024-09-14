2542d3624bb2f0f117ac6a9e31043c232bb02c71
Just R <remixingmagelang@gmail.com>
2024-09-15 03:01:24 +0700

    update Ah/plugins/gtpgo.py

--- Ah/plugins/gtpgo.py
+++ Ah/plugins/gtpgo.py
@@ -20,7 +20,7 @@ async def tanya(text):
 #    except requests.exceptions.RequestException as e:
 #        return f"Error: {str(e)}"
 
-@Client.on_message(filters.command("gtp", cmd))
+@Client.on_message(filters.command("gtp", cmd) & filters.me)
 async def gtp(client, message: Message):
     text = get_text(message)
     if not text:
