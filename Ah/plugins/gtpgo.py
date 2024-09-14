2142c02b8825e2be92367654f02e34ce1f7de5e0
Just R <remixingmagelang@gmail.com>
2024-09-14 18:54:55 +0700

    update Ah/plugins/gtpgo.py

--- Ah/plugins/gtpgo.py
+++ Ah/plugins/gtpgo.py
@@ -2,7 +2,6 @@ import requests
 
 async def tanya(text):
   url = "https://widipe.com/gptgo"
-  text = "resep seblak yang gurih kuah kare"
   params = {'text': text}
   headers = {'accept': 'application/json'}
   response = requests.get(url, headers=headers, params=params)
