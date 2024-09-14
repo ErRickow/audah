863ce565fb2095edd23d258652bf0dbd6379eccb
Just R <remixingmagelang@gmail.com>
2024-09-15 05:21:59 +0700

    update Ah/__init__.py

--- Ah/__init__.py
+++ Ah/__init__.py
@@ -67,6 +67,10 @@ class Ubot(Client):
 
         return decorator
 
+    async def start(self):
+        await super().start()
+        logger.info("Bot started")
+
 ubot = Ubot(
     name="sange",
     api_id=API_ID,
