6a8d10dce0c63198dd9229dd6d8d21c393412a66
Just R <remixingmagelang@gmail.com>
2024-09-15 00:03:35 +0700

    update Ah/plugins/help.py

--- Ah/plugins/help.py
+++ Ah/plugins/help.py
@@ -84,7 +84,7 @@ async def module_helper(client: Client, message: Message):
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await message.reply(
-            message, f"```{str(ac)}```\n• {client.me.mention} x {ubot.me.mention} •"
+            message, f"<blockquote>{str(ac)}</blockquote>\n• {client.me.mention} x {ubot.me.mention} •"
         )
         await message.reply(
             f"**Usage**:`.help broadcast` **To View Module details**"
