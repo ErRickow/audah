fdd217fb68f93f33a3841f2e683e13f26e19f72d
Just R <remixingmagelang@gmail.com>
2024-09-15 00:12:59 +0700

    update Ah/plugins/help.py

--- Ah/plugins/help.py
+++ Ah/plugins/help.py
@@ -84,7 +84,7 @@ async def module_helper(client: Client, message: Message):
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await message.reply(
-            f"<blockquote>{str(ac)}\n• {client.me.mention} x {ubot.me.mention} •</blockquote>"
+            f"<blockquote>{str(ac)}\n• {client.me.mention} x {ubot.me.mention} •</blockquote>", parse_mode=enums.ParseMode.HTML
         )
 
     if help_arg:
