77d3ae6d97e0747495f111666690dfe5c00ad61b
Just R <remixingmagelang@gmail.com>
2024-09-15 00:05:41 +0700

    update Ah/plugins/help.py

--- Ah/plugins/help.py
+++ Ah/plugins/help.py
@@ -54,12 +54,12 @@ async def module_help(client: Client, message: Message):
     if help_arg:
         if help_arg in CMD_HELP:
             commands: dict = CMD_HELP[help_arg]
-            this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
+            this_command = f"<blockquote>──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© er?"
+            this_command += "© er?</blockquote>"
             await edit_or_reply(
-                message, this_command, parse_mode=enums.ParseMode.MARKDOWN
+                message, this_command, parse_mode=enums.ParseMode.HTML
             )
         else:
             await edit_or_reply(
@@ -84,10 +84,7 @@ async def module_helper(client: Client, message: Message):
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await message.reply(
-            message, f"<blockquote>{str(ac)}</blockquote>\n• {client.me.mention} x {ubot.me.mention} •"
-        )
-        await message.reply(
-            f"**Usage**:`.help broadcast` **To View Module details**"
+            message, f"<blockquote>{str(ac)}\n• {client.me.mention} x {ubot.me.mention} •</blockquote>"
         )
 
     if help_arg:
