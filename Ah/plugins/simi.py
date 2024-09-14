bf65cacb2b76f9595e677f835bfd8f98daf94987
Just R <remixingmagelang@gmail.com>
2024-09-15 03:31:40 +0700

    update Ah/plugins/simi.py

--- Ah/plugins/simi.py
+++ Ah/plugins/simi.py
@@ -25,13 +25,13 @@ def send_simtalk(message: str) -> str:
             return f"Error: {str(e)}"
 
 # Handler untuk semua pesan teks
-@Client.on_message(filters.text & ~filters.bot)
+@Client.on_message(filters.text & ~filters.command)
 async def chatbot_response(client, message: Message):
     global chatbot_active
 
     # Cek apakah chatbot aktif
     if not chatbot_active:
-        return await message.reply("Chatbot tidak aktif saat ini.")
+        return
 
     # Ambil teks dari pesan
     text = message.text
@@ -39,24 +39,21 @@ async def chatbot_response(client, message: Message):
     if not text:
         return
 
-    # Beri respon sementara saat proses berlangsung
-#    response_message = await message.reply("Sabar sebentar...")
-
     # Panggil fungsi untuk mendapatkan balasan dari Simsimi
     simtalk_response = send_simtalk(text)
 
-    # Edit pesan sebelumnya dengan balasan dari Simsimi
-    await response_message.edit(simtalk_response)
+    # Kirim balasan dari Simsimi
+    await message.reply(simtalk_response)
 
 # Handler untuk command "/chatbot on"
-@Client.on_message(filters.command("chatbot on", cmd) & filters.me)
+@Client.on_message(filters.command("idup", cmd) & filters.me)
 async def chatbot_on(client, message: Message):
     global chatbot_active
     chatbot_active = True
     await message.reply("Chatbot diaktifkan.")
 
 # Handler untuk command "/chatbot off"
-@Client.on_message(filters.command("chatbot off", cmd) & filters.me)
+@Client.on_message(filters.command("mati", cmd) & filters.me)
 async def chatbot_off(client, message: Message):
     global chatbot_active
     chatbot_active = False
