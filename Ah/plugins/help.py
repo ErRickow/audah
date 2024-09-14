5ef85eceaf7ea5d98b30fadd3893f720fd949b99
Just R <remixingmagelang@gmail.com>
2024-09-14 23:48:19 +0700

    update Ah/plugins/help.py

--- Ah/plugins/help.py
+++ Ah/plugins/help.py
@@ -58,7 +58,7 @@ async def module_help(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© yes sir?"
+            this_command += "© er?"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
@@ -97,7 +97,7 @@ async def module_helper(client: Client, message: Message):
             this_command = f"<blockquote>──「 **Bantuan Untuk {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© Er?</blockquote>"
+            this_command += "© er?</blockquote>"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.HTML
             )
