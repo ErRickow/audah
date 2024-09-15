d6b6c3b1f8555a0c9548d73cef6830a013c833e5
Just R <remixingmagelang@gmail.com>
2024-09-15 10:45:07 +0700

    update Ah/__init__.py

--- Ah/__init__.py
+++ Ah/__init__.py
@@ -3,7 +3,6 @@ import logging
 import asyncio
 import sys
 import time
-import datetime
 from logging.handlers import RotatingFileHandler
 from aiohttp import ClientSession
 from pyrogram import Client
