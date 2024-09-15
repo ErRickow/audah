a181423dde04881cd1f3da0808db892555f8a722
Just R <remixingmagelang@gmail.com>
2024-09-15 19:50:55 +0700

    update Ah/plugins/simi.py

--- Ah/plugins/simi.py
+++ Ah/plugins/simi.py
@@ -55,7 +55,7 @@ async def chatbot_response(client, message: Message):
 
 
     text = message.text
-    if f"{PREFFIX}cmd" in text:
+    if f"{HNDLR}cmd" in text:
         logger.warning("Pesan kosong diterima, tidak ada yang bisa diproses.")
         return
 
