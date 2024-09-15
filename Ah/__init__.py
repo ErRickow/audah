91ebee2cfcde1d12762cc29b603fc18789201159
Just R <remixingmagelang@gmail.com>
2024-09-15 10:47:56 +0700

    update Ah/__init__.py

--- Ah/__init__.py
+++ Ah/__init__.py
@@ -88,16 +88,128 @@ ubot = Ubot(
 )
 
 # Membuat daftar bots berdasarkan session string
-bots = [
+bot1 = (
     Client(
-        name=f"bot{i+1}",
+        name="bot1",
         api_id=API_ID,
         api_hash=API_HASH,
-        session_string=globals().get(f"STRING_SESSION{i+1}"),
+        session_string=STRING_SESSION1,
         plugins=dict(root="Ah/plugins"),
     )
-    for i in range(10) if globals().get(f"STRING_SESSION{i+1}")
-]
+    if STRING_SESSION1
+    else None
+)
+
+bot2 = (
+    Client(
+        name="bot2",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION2,
+        plugins=dict(root="Ah/plugins"),
+    )
+    if STRING_SESSION2
+    else None
+)
+
+bot3 = (
+    Client(
+        name="bot3",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION3,
+        plugins=dict(root="Ah/plugins"),
+    )
+    if STRING_SESSION3
+    else None
+)
+
+bot4 = (
+    Client(
+        name="bot4",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION4,
+        plugins=dict(root="Ah/plugins"),
+    )
+    if STRING_SESSION4
+    else None
+)
+
+bot5 = (
+    Client(
+        name="bot5",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION5,
+        plugins=dict(root="Ah/plugins"),
+    )
+    if STRING_SESSION5
+    else None
+)
+
+bot6 = (
+    Client(
+        name="bot6",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION6,
+        plugins=dict(root="Ah/plugins"),
+    )
+    if STRING_SESSION6
+    else None
+)
+
+bot7 = (
+    Client(
+        name="bot7",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION7,
+        plugins=dict(root="Ah/plugins"),
+    )
+    if STRING_SESSION7
+    else None
+)
+
+bot8 = (
+    Client(
+        name="bot8",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION8,
+        plugins=dict(root="Ah/plugins"),
+    )
+    if STRING_SESSION8
+    else None
+)
+
+bot9 = (
+    Client(
+        name="bot9",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION9,
+        plugins=dict(root="Ah/plugins"),
+    )
+    if STRING_SESSION9
+    else None
+)
+
+bot10 = (
+    Client(
+        name="bot10",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION10,
+        plugins=dict(root="Ah/plugins"),
+    )
+    if STRING_SESSION10
+    else None
+)
+
+
+bots = [bot for bot in [bot1, bot2, bot3, bot4, bot5, bot6, bot7, bot8, bot9, bot10] if bot]
 
 # Fungsi untuk mengirim pesan dengan penanganan FloodWait
 async def send_message_with_floodwait_handling(client, chat_id, message):
