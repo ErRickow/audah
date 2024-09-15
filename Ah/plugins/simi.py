0c4eff016ecc1f2e53f01d05b4b8db2d5e62e880
Just R <remixingmagelang@gmail.com>
2024-09-16 00:50:02 +0700

    update Ah/plugins/simi.py

--- Ah/plugins/simi.py
+++ Ah/plugins/simi.py
@@ -60,7 +60,7 @@ async def chatbot_response(client, message):
     if "kemana lu" in text or "woi" in text:
         chatbot_active = True
         logger.info("Chatbot telah diaktifkan.")
-        await message.reply("hah?.")
+        await message.reply("hah?")
         return
 
     # Periksa perintah "up" untuk melakukan update userbot
