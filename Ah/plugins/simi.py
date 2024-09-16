8d360d84cd91368819ef7276635cad5b7ad3e20f
Just R <remixingmagelang@gmail.com>
2024-09-17 02:21:39 +0700

    update Ah/plugins/simi.py

--- Ah/plugins/simi.py
+++ Ah/plugins/simi.py
@@ -53,7 +53,7 @@ async def send_simtalk(message: str) -> str:
             return f"Error: {str(e)}"
 
 # Handler untuk semua pesan teks
-@ubot.on_message(filters.text & ~filters.bot & filters.me)
+@Clien.on_message(filters.text & ~filters.bot & filters.me)
 async def chatbot_response(client, message):
     global chatbot_active
 
