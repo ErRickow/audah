3bbf46567fae8def07f14be5db915070276d6f0a
Just R <remixingmagelang@gmail.com>
2024-09-16 23:36:28 +0700

    update Ah/plugins/simi.py

--- Ah/plugins/simi.py
+++ Ah/plugins/simi.py
@@ -131,4 +131,4 @@ async def chatbot_response(client, message):
     simtalk_response = await send_simtalk(text)
 
     # Mengirimkan respons kembali ke pengguna
-    await message.reply(f"<blockquote>AutoAi {client.me.mention}</blockquote>\n\n<pre>{simtalk_response}</pre>")
+    await message.reply(f"<blockquote>❏ AutoAi</blockquote>\n├• {client.me.mention}\n<pre>└• {simtalk_response}</pre>")
