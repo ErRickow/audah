57f4828819daed2f7055adc2a0e8e7c027e8bc1f
Just R <remixingmagelang@gmail.com>
2024-09-20 03:05:18 +0700

    update Ah/plugins/spam.py

--- Ah/plugins/spam.py
+++ Ah/plugins/spam.py
@@ -52,8 +52,8 @@ async def spam_cmd(client, message):
             except Exception as error:
                 return await spam.edit(error)
             await asyncio.sleep(1)
-            await message.delete()
-            await spam.delete()
+#            await message.delete()
+#            await spam.delete()
             for _ in range(quantity):
                 await client.send_message(message.chat.id, spam_text)
                 await asyncio.sleep(0.3)
