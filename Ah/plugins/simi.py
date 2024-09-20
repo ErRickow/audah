653e1bd5d2f74d25778f067bceeea64a9a6a24ba
Just R <remixingmagelang@gmail.com>
2024-09-20 10:48:36 +0700

    update Ah/plugins/simi.py

--- Ah/plugins/simi.py
+++ Ah/plugins/simi.py
@@ -48,7 +48,7 @@ def send_simtalk(message):
             return f"Error saat menghubungi API Simsimi: {str(e)}"
 
 # Handler untuk semua pesan teks
-@Client.on_message(filters.command(["er on", "diem", "woi", "cukup", "pull"], cmd) & filters.text & ~filters.bot & filters.me)
+@Client.on_message(filters.command(["er on", "diem", "woi", "cukup", "pull"], cmd_handler) & filters.text & ~filters.bot & filters.me)
 async def chatbot_response(client, message):
     global chatbot_active
 
